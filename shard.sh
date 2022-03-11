bddbbe41381609bd312dc174cd2a0f9e839bb865
Hi-DonnellyYellamae <74486004+Hi-DonnellyYellamae@users.noreply.github.com>
2022-03-11 23:37:54 +0800

    Add files via upload

---
+++ shard.sh
@@ -0,0 +1,153 @@
+#!/bin/bash
+tar -xf mongodb-linux-x86_64-rhel62-3.6.5.tgz 
+cp -r  mongodb-linux-x86_64-rhel62-3.6.5/ /mongodb/
+useradd -r mongod -s /sbin/nologin
+chown -R mongod:mongod /mongodb/
+echo 'export PATH=/mongodb/bin:$PATH' >> /etc/profile
+source /etc/profile
+
+# shard1 conf
+# 一主两从，其中一个节点为arbiter，复制集名字repo1
+for port in {27017,27018,27019}
+do
+mkdir -pv /mongodb/${port}/{db,log,conf}
+cat > /mongodb/${port}/conf/mongodb.conf << EOF
+systemLog:
+  destination: file
+  path: /mongodb/${port}/log/mongodb.log   
+  logAppend: true
+storage:
+  journal:
+    enabled: true
+  dbPath: /mongodb/${port}/db
+  directoryPerDB: true
+  wiredTiger:
+    engineConfig:
+      cacheSizeGB: 1
+      directoryForIndexes: true
+    collectionConfig:
+      blockCompressor: zlib
+    indexConfig:
+      prefixCompression: true
+net:
+  bindIp: 192.168.180.10,127.0.0.1
+  port: ${port}
+replication:
+  oplogSizeMB: 2048
+  replSetName: repl1
+sharding:
+  clusterRole: shardsvr
+processManagement: 
+  fork: true
+EOF
+chown -R mongod:mongod /mongodb/
+mongod -f /mongodb/${port}/conf/mongodb.conf
+done
+mongo 127.0.0.1:27017/admin --eval 
+"rs.initiate({_id: 'repl1', members: [ {_id: 0, host: '192.168.180.10:27017'}, {_id: 1, host: '192.168.180.10:27018'}, {_id: 2, host: '192.168.180.10:27019',"arbiterOnly":true}] })"
+
+# shard2 conf
+# 一主两从，其中一个节点为arbiter，复制集名字repo2
+for port in {37017,37018,37019}
+do
+mkdir -pv /mongodb/${port}/{db,log,conf}
+chown -R mongod:mongod /mongodb/${port}
+cat > /mongodb/$port/conf/mongodb.conf << EOF
+systemLog:
+  destination: file
+  path: /mongodb/${port}/log/mongodb.log   
+  logAppend: true
+storage:
+  journal:
+    enabled: true
+  dbPath: /mongodb/${port}/db
+  directoryPerDB: true
+  wiredTiger:
+    engineConfig:
+      cacheSizeGB: 1
+      directoryForIndexes: true
+    collectionConfig:
+      blockCompressor: zlib
+    indexConfig:
+      prefixCompression: true
+net:
+  bindIp: 192.168.180.10,127.0.0.1
+  port: ${port}
+replication:
+  oplogSizeMB: 2048
+  replSetName: repl2
+sharding:
+  clusterRole: shardsvr
+processManagement: 
+  fork: true
+EOF
+mongod -f /mongodb/${port}/conf/mongodb.conf
+done
+mongo 127.0.0.1:37017/admin --eval "rs.initiate({_id: 'repl2', members: [ {_id: 0, host: '192.168.180.10:37017'}, {_id: 1, host: '192.168.180.10:37018'}, {_id: 2, host: '192.168.180.10:37019',"arbiterOnly":true}] })"
+
+# config Server不支持arbiter
+# config Server conf
+for port in {47017,47018,47019}
+do
+mkdir -pv /mongodb/${port}/{db,log,conf}
+chown -R mongod:mongod /mongodb/${port}
+cat > /mongodb/${port}/conf/mongodb.conf <<EOF
+systemLog:
+  destination: file
+  path: /mongodb/${port}/log/mongodb.conf
+  logAppend: true
+storage:
+  journal:
+    enabled: true
+  dbPath: /mongodb/${port}/db
+  directoryPerDB: true
+  #engine: wiredTiger
+  wiredTiger:
+    engineConfig:
+      cacheSizeGB: 1
+      directoryForIndexes: true
+    collectionConfig:
+      blockCompressor: zlib
+    indexConfig:
+      prefixCompression: true
+net:
+  bindIp: 192.168.180.10,127.0.0.1
+  port: $port
+replication:
+  oplogSizeMB: 2048
+  replSetName: configReplSet
+sharding:
+  clusterRole: configsvr
+processManagement: 
+  fork: true
+EOF
+mongod -f /mongodb/$port/conf/mongodb.conf
+done
+mongo 127.0.0.1:47017/admin --eval "rs.initiate({_id: 'configReplSet', members: [ {_id: 0, host: '192.168.180.10:47017'}, {_id: 1, host: '192.168.180.10:47018'}, {_id: 2, host: '192.168.180.10:47019'}] })"
+
+# mongos nodes
+port=57017
+mkdir -pv /mongodb/${port}/{log,conf}
+chown -R mongod:mongod /mongodb/${port}
+cat > /mongodb/$port/conf/mongodb.conf << EOF
+systemLog:
+  destination: file
+  path: /mongodb/$port/conf/mongodb.conf
+  logAppend: true
+net:
+  bindIp: 192.168.180.10,127.0.0.1
+  port: $port
+sharding:
+  configDB: configReplSet/192.168.180.10:47017,192.168.180.10:47018,192.168.180.10:47019
+processManagement: 
+  fork: true
+EOF
+
+mongos -f /mongodb/$port/conf/mongodb.conf
+
+# 添加分片
+mongo 127.0.0.1:57017/admin --eval "db.runCommand( { addshard : 'repl1/192.168.180.10:27017,192.168.180.10:27018,192.168.180.10:27019',name:'shard1'} )"
+mongo 127.0.0.1:57017/admin --eval "db.runCommand( { addshard : 'repl2/192.168.180.10:37017,192.168.180.10:37018,192.168.180.10:37019',name:'shard2'} )"
+
+# 列出分片
+mongo 127.0.0.1:57017/admin --eval "db.runCommand( { listshards : 1 } )"
